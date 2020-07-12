6e270192d15e406934fa014f3423931d1c22568c
root <root@shak48.home>
2020-07-12 09:23:49 -0700

    First commit

---
+++ LoginDemo/src/main/java/com/webapps/LoginDemo/App.java
@@ -0,0 +1,13 @@
+package com.webapps.LoginDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
